634b858ed4cf5d20b0e127d632fdd5086e2e298e
SPSPRANAV <46101465+SPSPRANAV@users.noreply.github.com>
2020-07-28 15:06:35 +0530

    Update views.py
    Updated views for creating form

--- crawler/views.py
+++ crawler/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect,HttpResponse
+import bs4
+import requests
+from .forms import HandleForm
 # Create your views here.
+
+def home(request):
+    if request.method=='POST':
+        form=HandleForm(request.POST)
+        if form.is_valid():
+            handle=form.cleaned_data.get('handle')
+            return redirect('/display/'+handle)
+    else:
+            form=HandleForm()
+
+    return render(request,'crawler/home.html',{'form':form})
+
+def display(request,handle):
+    context={
+        'handle':handle,
+    }
+    return render(request,'crawler/display.html',context)
